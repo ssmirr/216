f5b74a06c99faab0efc462908106e98d5de2779b
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2021-05-28 16:04:53 -0400

    adding mvn project

---
+++ src/main/java/edu/ncsu/csc216/array_list/ArrayIntList.java
@@ -0,0 +1,67 @@
+package edu.ncsu.csc216.array_list;
+
+import java.util.Arrays;
+
+public class ArrayIntList {
+
+	private static final int DEFAULT_SIZE = 10;
+	private int[] list;
+	private int size;
+	
+	public ArrayIntList(int capacity) {
+		list = new int[capacity];
+		size = 0;
+	}
+	
+	public ArrayIntList() {
+		this(DEFAULT_SIZE);
+	}
+	
+	public int size() {
+		return size;
+	}
+
+	public void add(int value) {
+		ensureCapacity(size + 1);
+		list[size] = value;
+		size++;
+	}
+	
+	public void add(int idx, int value) {
+		ensureCapacity(size + 1);
+		for (int i = size - 1; i >= idx; i--) {
+			list[i + 1] = list[i];
+		}
+		list[idx] = value;
+		size++;
+	}
+
+	public int get(int idx) {
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return list[idx];
+	}
+	
+	private void ensureCapacity(int capacity) {
+		if (capacity > list.length) {
+			int newCapacity = list.length * 2 + 1;
+			if (capacity > newCapacity) {
+				newCapacity = capacity;
+				
+			}
+			list = Arrays.copyOf(list,  newCapacity);
+		}
+	}
+
+	public int remove(int idx) {
+		int rtn = list[idx];
+		for (int i = idx; i < size - 1; i++) {
+			list[i] = list[i+1];
+		}
+		list[size-1] = 0;
+		size--;
+		return rtn;
+	}
+
+}
