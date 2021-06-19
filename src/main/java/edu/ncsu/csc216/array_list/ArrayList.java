47abe5c5125aea8cbe46798e9d061fb6a4e22dd0
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2021-06-19 14:44:16 -0400

    adding arraylist

--- src/main/java/edu/ncsu/csc216/array_list/ArrayList.java
+++ src/main/java/edu/ncsu/csc216/array_list/ArrayList.java
@@ -1,21 +1,25 @@
 package edu.ncsu.csc216.array_list;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-import edu.ncsu.csc216.list.IntList;
+import edu.ncsu.csc216.list.List;
 
-public class ArrayIntList implements IntList {
+public class ArrayList<E> implements List<E> {
 
 	private static final int DEFAULT_SIZE = 10;
-	private int[] list;
+	private E[] list;
 	private int size;
 	
-	public ArrayIntList(int capacity) {
-		list = new int[capacity];
+	@SuppressWarnings("unchecked")
+	public ArrayList(int capacity) {
+//		list = new int[capacity];
+		list = (E[]) (new Object[capacity]);
 		size = 0;
 	}
 	
-	public ArrayIntList() {
+	public ArrayList() {
 		this(DEFAULT_SIZE);
 	}
 	
@@ -23,13 +27,13 @@ public class ArrayIntList implements IntList {
 		return size;
 	}
 
-	public void add(int value) {
+	public void add(E value) {
 		ensureCapacity(size + 1);
 		list[size] = value;
 		size++;
 	}
 	
-	public void add(int idx, int value) {
+	public void add(int idx, E value) {
 		ensureCapacity(size + 1);
 		for (int i = size - 1; i >= idx; i--) {
 			list[i + 1] = list[i];
@@ -44,7 +48,7 @@ public class ArrayIntList implements IntList {
 	 * @return
 	 * @throws IndexOutOfBoundsException when index is not between 0 and size-1, inclusive
 	 */
-	public int get(int idx) {
+	public E get(int idx) {
 		if (idx < 0 || idx >= size) {
 			throw new IndexOutOfBoundsException("" + idx);
 		}
@@ -62,18 +66,18 @@ public class ArrayIntList implements IntList {
 		}
 	}
 
-	public int remove(int idx) {
-		int rtn = list[idx];
+	public E remove(int idx) {
+		E rtn = list[idx];
 		for (int i = idx; i < size - 1; i++) {
 			list[i] = list[i+1];
 		}
-		list[size - 1] = 0;
+		list[size - 1] = null;
 		size--;
 		return rtn;
 	}
 
 	@Override
-	public int indexOf(int value) {
+	public int indexOf(E value) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -85,15 +89,49 @@ public class ArrayIntList implements IntList {
 	}
 
 	@Override
-	public void set(int idx, int value) {
+	public void set(int idx, E value) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public boolean contains(int value) {
+	public boolean contains(E value) {
 		// TODO Auto-generated method stub
 		return false;
-	}
+	}	
 
+	@Override
+	public Iterator<E> iterator() {
+		return new ArrayIterator();
+	}
+	
+	private class ArrayIterator implements Iterator<E> {
+
+		private int idx;
+		
+		public ArrayIterator() {
+			idx = 0;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return idx < size();
+		}
+
+		@Override
+		public E next() {
+			idx++;
+			if (idx >= size) {
+				throw new NoSuchElementException();
+			}
+			return get(idx-1);
+		}
+
+		@Override
+		public void remove() {
+			ArrayList.this.remove(idx-1);
+			idx--;
+		}
+		
+	}
 }
